234b41ec754483ad9d128cfd054e217f76017f76
Riley Eynon-Lynch <riley@peardeck.com>
2017-05-29 07:27:27 -0500

    Remove ugly hud thing

--- client/src/Board.js
+++ client/src/Board.js
@@ -63,16 +63,17 @@ function drawHud(localPlayer, gameState) {
   
   const hudElements = [];
 
-  if (gameState.ballAttraction.lodgedInPlayer === localPlayer.playerId) {
-    hudElements.push(<g className='fadeIn' transform={`translate(${localPlayer.body.position[0]} ${localPlayer.body.position[1]}) rotate(${localPlayer.body.angle / Math.PI * 180})`} key={localPlayer.playerId} opacity="0.5">
-      <line x1="0" y1="-400" x2="0" y2="400" stroke="gray" strokeWidth="0.05" strokeDasharray="1, 1" />
-      <line x1="-400" y1="0" x2="400" y2="0" stroke="gray" strokeWidth="0.05" strokeDasharray="1, 1" />
-      <text transform={`translate(0, 20) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">W</text>
-      <text transform={`translate(0, -20) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">S</text>
-      <text transform={`translate(20, 0) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">D</text>
-      <text transform={`translate(-20, 0) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">A</text>
-    </g>);
-  }
+  // This is maybe helpful for learning controls but too ugly to bear
+  // if (gameState.ballAttraction.lodgedInPlayer === localPlayer.playerId) {
+  //   hudElements.push(<g className='fadeIn' transform={`translate(${localPlayer.body.position[0]} ${localPlayer.body.position[1]}) rotate(${localPlayer.body.angle / Math.PI * 180})`} key={localPlayer.playerId} opacity="0.5">
+  //     <line x1="0" y1="-400" x2="0" y2="400" stroke="gray" strokeWidth="0.05" strokeDasharray="1, 1" />
+  //     <line x1="-400" y1="0" x2="400" y2="0" stroke="gray" strokeWidth="0.05" strokeDasharray="1, 1" />
+  //     <text transform={`translate(0, 20) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">W</text>
+  //     <text transform={`translate(0, -20) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">S</text>
+  //     <text transform={`translate(20, 0) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">D</text>
+  //     <text transform={`translate(-20, 0) rotate(${-localPlayer.body.angle / Math.PI * 180}) scale(.1, -.1)`} textAnchor="middle" alignmentBaseline="central">A</text>
+  //   </g>);
+  // }
 
   return hudElements;
 }
